d545aa6604f8c06165a44f667da030f22c69ab3d
Shipra <code.shipra@gmail.com>
2021-03-21 20:18:58 +0530

    adds dockerfile and github workflow

--- social_connect/settings.py
+++ social_connect/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "-oolx&#0ty125q%(q6dy$5@!7jj+o7+x14c0&+-2dn2tthlz30"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", False))
 
 ALLOWED_HOSTS = []
 
@@ -85,13 +85,13 @@ WSGI_APPLICATION = "social_connect.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         "TEST": {
-            "NAME": os.environ.get("DB_TESTDATABASENAME", "testdatabase"),
+            "NAME": os.getenv("DB_TESTDATABASENAME", "testdatabase"),
         },
     }
 }
